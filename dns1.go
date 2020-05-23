b7ff59ca2497bf6fb236564589a08b41e9badd91
zhanghaoliang <12441409@qq.com>
2020-05-23 22:01:21 +0800

    减少不必要的函数

--- dns1.go
+++ dns1.go
@@ -137,16 +137,8 @@ func NewAResource(query dnsmessage.Name, a [4]byte) dnsmessage.Resource {
 }
 
 
-func ReverseString(s string) string {
-        runes := []rune(s)
-        for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
-                runes[from], runes[to] = runes[to], runes[from]
-        }
-        return string(runes)
 
-}
 func InetAtoN(ip string) int64 {
-//    ip1 := ReverseString(ip)  //解决反序列的问题
     ret := big.NewInt(0)
     ret.SetBytes(net.ParseIP(ip).To4())
     return ret.Int64()
